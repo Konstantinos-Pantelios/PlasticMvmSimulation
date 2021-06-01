8dcf18e24ef2e0f9131173b5e6a90e5488658f63
Konstantinos Pantelios <konstantinospantelios@yahoo.com>
2021-06-01 13:57:12 +0200

    Add figures - Relative angles need fixing

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
+import math
 import networkx as nx
 import matplotlib.pyplot as plt
 import classes as pls
 import simulation as sim
+import numpy as np
 
 
 
@@ -11,9 +13,13 @@ edges_path = "./Data/test_network.shp"
 nodes_path = "./Data/test_nodes.shp"
 
 
+
+
+
 #Read the shp files 
 G = nx.read_shp(edges_path,simplify=False,strict=False) # Water network graph (Contains ALL nodes between different edges that are irrelevant for the project)
 N = nx.read_shp(nodes_path,simplify=False,strict=False) # Relevant to project node graph (DOES NOT contain any edges)
+im = plt.imread('Data/compass_PNG25581.png',format='png')
 
 pos_e = {k:v for k,v in enumerate(G.nodes())} # Get enumerated position of nodes. Dictionary {0:(x0,y0),1:(x1,y1),...}
 pos_n = {k:v for k,v in enumerate(N.nodes())} # Get enumerated position of nodes. Dictionary {0:(x0,y0),1:(x1,y1),...} 
@@ -28,7 +34,7 @@ firstnode = pos_e[0]
 
 #######################-------------- Instantiate Objects ---------------##########################
 #Create 5 "plastic" objects at x:0 ,y:0
-plastics_100 = pls.create_plastics(5)
+plastics_100 = pls.create_plastics(100)
 
 #Create n "node" objects. n = G.number_of_nodes() 
 nodes = {}
@@ -74,7 +80,14 @@ for plastic_unit in plastics_100:
 # print(nodes[0].fields['class'])
 ##############################################
 
-sim.simulation(G,nodes,plastics_100)
+wind_direction=20
+
+min_x=np.min([n[0] for n in nodes.keys()])
+min_y=np.min([n[1] for n in nodes.keys()])
+max_x=np.max([n[0] for n in nodes.keys()])
+max_y=np.max([n[1] for n in nodes.keys()])
+dx = 80 * math.sin(math.radians(wind_direction))
+dy = 80 * math.cos(math.radians(wind_direction))
 
 
 #Display network graph figure
@@ -82,16 +95,46 @@ pos_pls =  {k:v.coords() for k,v in enumerate(plastics_100)} # Get enumerated po
 pos_relabel = {k:v.has_plastics_num() for k,v in enumerate(nodes.values())} # Get enumerated amount of plastic units in nodes. Dictionary {0:5,1:4,2:0,..}
 pos_node ={k:v.coords() for k,v in enumerate(nodes.values())} # Get enumerated position of nodes. Dictionary {0:(x0,y0),1:(x1,y1),...}
 
+X=nx.MultiGraph()
+X.add_nodes_from(pos_pls.keys())
+l = [set(x) for x in G.edges()]
+edg=[tuple(k for k, v in pos_e.items() if v in s1 ) for s1 in l]
+
+plt.subplot(121)
+plt.arrow(x=min_x-70, y=min_y-80, dx=0, dy=150, width=1) 
+plt.arrow(x=min_x-70, y=min_y-80, dx=dx, dy=dy, width=10)
+plt.axis([min_x-200,max_x+200,min_y-200,max_y+200])
+plt.annotate("Wind Direction: "+str(wind_direction)+" degrees", xy=(min_x-60, min_y-100))
+plt.annotate("N ", xy=(min_x-70+10, min_y-80+160))
+
+nx.draw_networkx_nodes(X, pos_pls,node_size=10)
+nx.draw_networkx_labels(X, pos_node, labels=pos_relabel,font_size=16,horizontalalignment='right', verticalalignment='bottom')
+X.add_edges_from(edg)
+nx.draw_networkx_edges(X, pos_e)
+
+
+sim.simulation(G,nodes,plastics_100,wind_direction)
+
+#Display network graph figure
+pos_pls =  {k:v.coords() for k,v in enumerate(plastics_100)} # Get enumerated position of plastic units. Dictionary {0:(x1,y1),1:(x2,y2),...} 
+pos_relabel = {k:v.has_plastics_num() for k,v in enumerate(nodes.values())} # Get enumerated amount of plastic units in nodes. Dictionary {0:5,1:4,2:0,..}
+pos_node ={k:v.coords() for k,v in enumerate(nodes.values())} # Get enumerated position of nodes. Dictionary {0:(x0,y0),1:(x1,y1),...}
 
 X=nx.MultiGraph()
 X.add_nodes_from(pos_pls.keys())
 l = [set(x) for x in G.edges()]
 edg=[tuple(k for k, v in pos_e.items() if v in s1 ) for s1 in l]
 
+plt.subplot(122)
+plt.arrow(x=min_x-70, y=min_y-80, dx=0, dy=150, width=1) 
+plt.arrow(x=min_x-70, y=min_y-80, dx=dx, dy=dy, width=10) 
+plt.axis([min_x-200,max_x+200,min_y-200,max_y+200])
+plt.annotate("Wind Direction: "+str(wind_direction)+" degrees", xy=(min_x-60, min_y-100))
+plt.annotate("N ", xy=(min_x-70+10, min_y-80+160))
 
 nx.draw_networkx_nodes(X, pos_pls,node_size=10)
 nx.draw_networkx_labels(X, pos_node, labels=pos_relabel,font_size=16,horizontalalignment='right', verticalalignment='bottom')
 X.add_edges_from(edg)
 nx.draw_networkx_edges(X, pos_e)
-plt.show()
 
+plt.show()
--- simulation.py
+++ simulation.py
@@ -2,7 +2,7 @@ import networkx as nx
 import classes as pls
 import random
 
-def simulation(graph,nodes,plastics):
+def simulation(graph,nodes,plastics,wind):
     
     #plastics = [pls_object1, pls_object2,...]
     #nodes = {(x1,y1):pls_object1, (x2,y2):node_object2} Both relevant and irrelevant
@@ -11,7 +11,7 @@ def simulation(graph,nodes,plastics):
     #graph.edges(data=True) = [((x_start,y_start),(x_end,y_end),{.......}),....]
     #wind_angle = 30 #in degrees fron North CW
 
-    for minute in range(60): # for every minute in an hour 
+    for minute in range(1): # for every minute in an hour 
         print("We are at the ",minute, "minute.")
         for plastic_unit in plastics: #for every plastic unit
             print("We are at plastic:",plastic_unit)
@@ -22,7 +22,7 @@ def simulation(graph,nodes,plastics):
                 print("we are at neighbor:", nodes[neigh])
                 distance = pls.distance(node_coords,neigh)
                 edge_angle = pls.angle(node_coords,neigh)
-                wind_angle = 0
+                wind_angle = wind
                 relative_angle=0
                 
                 if edge_angle[1]==1:
@@ -30,7 +30,7 @@ def simulation(graph,nodes,plastics):
                 elif edge_angle[1]==2:
                     wind_angle=wind_angle-90
                     if wind_angle<0:
-                        relative_angle = abs(360+wind_angle - edge_angle[0])
+                        relative_angle = abs(360-wind_angle + edge_angle[0])
                     else:
                         relative_angle =  abs(wind_angle - edge_angle[0])
                 elif edge_angle[1]==3:
@@ -50,17 +50,17 @@ def simulation(graph,nodes,plastics):
                 chance = random.randint(1,100)
                 print("Relative angle:",relative_angle,"chance:",chance)
                 if relative_angle>=0 and relative_angle<=20:
-                    if chance <= 90:
+                    if chance <= 95:
                         is_in_node.remove_plastic(plastic_unit)
                         nodes[neigh].insert_plastic(plastic_unit)
                         break
                 elif relative_angle>20 and relative_angle <=45:
-                    if chance <= 70:
+                    if chance <= 60:
                         is_in_node.remove_plastic(plastic_unit)
                         nodes[neigh].insert_plastic(plastic_unit)
                         break
                 elif relative_angle>45 and relative_angle <=90:
-                    if chance <= 50:
+                    if chance <= 40:
                         is_in_node.remove_plastic(plastic_unit)
                         nodes[neigh].insert_plastic(plastic_unit)
                         break    
