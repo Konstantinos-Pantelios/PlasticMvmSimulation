813cf858a907721591623768857968e28a6c1df6
Konstantinos Pantelios <konstantinospantelios@yahoo.com>
2021-06-06 16:45:17 +0200

    add ground_truth region and multiple plastic "gates"

--- main.py
+++ main.py
@@ -12,41 +12,46 @@ def cls():
     os.system('cls' if os.name=='nt' else 'clear')
     print("################ Plastic Movement Simulator ################"+"\n"+ "##------   TUDelft - Noria Sustainable Innovators   ------##"+"\n"+"##------      Synthesis project GEO1101 - 2021      ------##"+"\n"+"############################################################"+"\n")
 
-def params():
+def params(lever):
     print("################ Plastic Movement Simulator ################"+"\n"+ "##------   TUDelft - Noria Sustainable Innovators   ------##"+"\n"+"##------      Synthesis project GEO1101 - 2021      ------##"+"\n"+"############################################################"+"\n")
-    while True:
-        try:
-            Wind = int(input("Insert wind direction (in degrees): "))
-            assert 0 <= Wind <= 359 
-        except ValueError:
-            cls()
-            print("This is not a valid number. Try again.")
-        except:
-            cls()
-            print("Direction should be in degrees (0-359). Try again.")
-        else: 
-            while True:    
-                try:
-                    Amount = int(input("Insert amount of plastic units: "))
-                    assert 1 <= Amount <= 1000 
-                except ValueError:
-                    cls()
-                    print("This is not a valid number. Try again.")
-                except:
-                    cls()
-                    print("Too large or small amount (1-1000). Try again")
-                else:
-                    break
-            return Wind,Amount
+    if lever:
+        while True:
+            try:
+                Wind = int(input("Insert wind direction (in degrees): "))
+                assert 0 <= Wind <= 359 
+            except ValueError:
+                cls()
+                print("This is not a valid number. Try again.")
+            except:
+                cls()
+                print("Direction should be in degrees (0-359). Try again.")
+            else: 
+                while True:    
+                    try:
+                        Amount = int(input("Insert amount of plastic units: "))
+                        assert 1 <= Amount <= 1000 
+                    except ValueError:
+                        cls()
+                        print("This is not a valid number. Try again.")
+                    except:
+                        cls()
+                        print("Too large or small amount (1-1000). Try again")
+                    else:
+                        break
+                return Wind
+    else: return 45
         
 
 
-Wind, Amount = params() 
+Wind = params(0) #Change this to 1 to allow the user to insert specific wind direction
+print("|NOTES:")
+print("|-> The simulation works by assuming a wind direction of 45 degrees (SW)")
+print("|-> Plastiscs are being inserted into the network's nodes based on proximity to 'recreation' locations (1 to 1)")
 start_t=time.perf_counter() #initiate timer
 
 #Define the local path of the shapefile  
-edges_path = "./Data/delft_waterlines.shp"
-nodes_path = "./Data/delft_nodes.shp"
+edges_path = "./Data/region_delft_waterlines.shp"
+nodes_path = "./Data/region_delft_nodes2.shp"
 
 
 #Read the shp files 
@@ -73,7 +78,14 @@ firstnode = pos_e[25]
 
 #######################-------------- Instantiate Objects ---------------##########################
 #Create n number of "plastic" objects at x:0 ,y:0
-plastics_100 = pls.create_plastics(Amount)
+n=0
+for node in N.nodes.items():
+    if not node[1]["pls_amount"]:
+        node[1]["pls_amount"]=0
+    n+=node[1]["pls_amount"]
+
+plastics_100 = pls.create_plastics(n)
+plastics_to_pour = plastics_100.copy()
 
 #Create n "node" objects. n = G.number_of_nodes() 
 nodes = {}
@@ -85,6 +97,8 @@ for d in G.nodes.items():
         y_coord = d[0][1]
         attributes = {k:relevant_nodes[d[0]][k] for k in relevant_nodes[d[0]].keys()}
         nodes[d[0]]=pls.node(identification,x_coord,y_coord,attributes) #Instantiate decision making node object inside a dictionary {(x1,y1):<node_object>,..}
+        for k in range(int(relevant_nodes[d[0]]["pls_amount"])):
+            nodes[d[0]].insert_plastic(plastics_to_pour.pop(-1))
     else:
         identification = str(list(pos_e.keys())[list(pos_e.values()).index(d[0])])
         x_coord = d[0][0]
@@ -92,13 +106,12 @@ for d in G.nodes.items():
         attributes = d[1]
         nodes[d[0]]=pls.node(identification,x_coord,y_coord,attributes) #Instantiate irrelevant node object inside a dictionary {(x1,y1):<node_object>,..}
 
+
 #######################--------------------------------------------------##########################
 
 #Pour all of the plastics into the first node of the water network. max value: size(nodes)-2
 
-for plastic_unit in plastics_100:
-    nodes[firstnode].insert_plastic(plastic_unit)
-    plastic_unit.has_visited(nodes[firstnode])
+# 
 
 ##############################################
 
@@ -109,10 +122,10 @@ min_x=np.min([n[0] for n in nodes.keys()])
 min_y=np.min([n[1] for n in nodes.keys()])
 max_x=np.max([n[0] for n in nodes.keys()])
 max_y=np.max([n[1] for n in nodes.keys()])
-dx_wind = 80 * math.sin(math.radians(wind_direction))
-dy_wind = 80 * math.cos(math.radians(wind_direction))
-dx_leeway = 80 * math.sin(math.radians(wind_direction+leeway_drift))
-dy_leeway = 80 * math.cos(math.radians(wind_direction+leeway_drift))
+dx_wind = 480 * math.sin(math.radians(wind_direction))
+dy_wind = 480 * math.cos(math.radians(wind_direction))
+dx_leeway = 480 * math.sin(math.radians(wind_direction+leeway_drift))
+dy_leeway = 480 * math.cos(math.radians(wind_direction+leeway_drift))
 
 #Display network graph figure
 pos_pls =  {k:v.coords() for k,v in enumerate(plastics_100)}
@@ -128,13 +141,13 @@ edg=[tuple(k for k, v in pos_e.items() if v in s1 ) for s1 in l]
 
 plt.subplot(121)
 blue_patch = mpatches.Patch(color='tab:blue', label="Plastics in nodes")
-plt.arrow(x=min_x-70, y=min_y-80, dx=0, dy=150, width=1) 
+plt.arrow(x=min_x-70, y=min_y-80, dx=0, dy=500, width=1) 
 plt.arrow(x=min_x-70, y=min_y-80, dx=dx_wind, dy=dy_wind, width=10,label="asdsad")
 plt.arrow(x=min_x-70, y=min_y-80, dx=dx_leeway, dy=dy_leeway, width=5,color='r')
 plt.axis([min_x-200,max_x+200,min_y-200,max_y+200])
 plt.annotate("Wind Direction: "+str(wind_direction)+" degrees", xy=(min_x+40, min_y-100))
 plt.annotate("N ", xy=(min_x-70+10, min_y-80+160))
-plt.annotate("Leeway drift +"+str(leeway_drift)+" degrees", xy=(min_x+40, min_y-140))
+plt.annotate("Leeway drift +"+str(leeway_drift)+" degrees", xy=(min_x+40, min_y-150))
 plt.legend(handles=[blue_patch])
 
 nx.draw_networkx_nodes(X, pos_pls, node_size=node_plastic_count)
@@ -175,19 +188,19 @@ edg=[tuple(k for k, v in pos_e2.items() if v in s1 ) for s1 in l]
 
 plt.subplot(122)
 red_patch = mpatches.Patch(color='red', label="Plastics that haven't yet reached a particular node")
-plt.arrow(x=min_x-70, y=min_y-80, dx=0, dy=150, width=1) 
+plt.arrow(x=min_x-70, y=min_y-80, dx=0, dy=500, width=1) 
 plt.arrow(x=min_x-70, y=min_y-80, dx=dx_wind, dy=dy_wind, width=10)
 plt.arrow(x=min_x-70, y=min_y-80, dx=dx_leeway, dy=dy_leeway, width=5,color='r')
 plt.axis([min_x-200,max_x+200,min_y-200,max_y+200])
 plt.annotate("Wind Direction: "+str(wind_direction)+" degrees", xy=(min_x+40, min_y-100))
 plt.annotate("N ", xy=(min_x-70+10, min_y-80+160))
-plt.annotate("Leeway drift +"+str(leeway_drift)+" degrees", xy=(min_x+40, min_y-140))
-plt.legend(handles=[blue_patch,red_patch])
+plt.annotate("Leeway drift +"+str(leeway_drift)+" degrees", xy=(min_x+40, min_y-150))
+plt.legend(handles=[blue_patch])
 
 nx.draw_networkx_nodes(X, pos_pls, nodelist=pos_pls_re, node_color='red', node_size=2,node_shape='*')
 nx.draw_networkx_nodes(X, pos_node, nodelist=pos_relabel, node_size=node_plastic_count)
 
-#nx.draw_networkx_labels(X, pos_node, labels=pos_relabel, horizontalalignment="left",verticalalignment="bottom")
+nx.draw_networkx_labels(X, pos_node, labels=pos_relabel, horizontalalignment="left",verticalalignment="bottom")
 X.add_edges_from(G.edges())
 nx.draw_networkx_edges(X, pos_e2)
 print("\nExecution time: "+str(round(time.perf_counter()-start_t,2))+"sec")
